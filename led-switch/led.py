e77a0848907f1aef0cd3e0f66b898f8d7f9fcc5a
phi-line <phi.liney@gmail.com>
2017-12-26 02:42:46 +0000

    added blinking led

--- led-switch/led.py
+++ led-switch/led.py
@@ -1,12 +1,16 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
-print 'LED on'
-GPIO.output(18,GPIO.HIGH)
-time.sleep(1)
-print 'LED off'
-GPIO.output(18,GPIO.LOW)
+def main():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(16,GPIO.OUT)
 
+    while True:
+	time.sleep(0.2)
+    	GPIO.output(16,GPIO.HIGH)
+    	time.sleep(0.2)
+        GPIO.output(16,GPIO.LOW)
+
+if __name__=='__main__':
+    main()
